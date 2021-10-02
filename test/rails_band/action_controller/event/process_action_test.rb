9e26164754bbdc6b1e03fdc52123ab3b387180c1
Yutaka Kamei <kamei@yykamei.me>
2021-10-02 22:38:45 +0900

    Test attributes of BaseEvent

--- test/rails_band/action_controller/event/process_action_test.rb
+++ test/rails_band/action_controller/event/process_action_test.rb
@@ -10,6 +10,51 @@ class ProcessActionTest < ActionDispatch::IntegrationTest
     }
   end
 
+  test 'returns name' do
+    get '/users'
+    assert_equal 'process_action.action_controller', @event.name
+  end
+
+  test 'returns time' do
+    get '/users'
+    assert_instance_of Float, @event.time
+  end
+
+  test 'returns end' do
+    get '/users'
+    assert_instance_of Float, @event.end
+  end
+
+  test 'returns transaction_id' do
+    get '/users'
+    assert_instance_of String, @event.transaction_id
+  end
+
+  test 'returns children' do
+    get '/users'
+    assert_equal [], @event.children
+  end
+
+  test 'returns cpu_time' do
+    get '/users'
+    assert_instance_of Float, @event.cpu_time
+  end
+
+  test 'returns idle_time' do
+    get '/users'
+    assert_instance_of Float, @event.idle_time
+  end
+
+  test 'returns allocations' do
+    get '/users'
+    assert_instance_of Integer, @event.allocations
+  end
+
+  test 'returns duration' do
+    get '/users'
+    assert_instance_of Float, @event.duration
+  end
+
   test 'returns an instance of ProcessAction' do
     get '/users'
     assert_instance_of RailsBand::ActionController::Event::ProcessAction, @event
