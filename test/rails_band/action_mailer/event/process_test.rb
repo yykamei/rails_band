10673ffeb458dae4f5600190b9e6f085ae4538f3
Yutaka Kamei <kamei@yykamei.me>
2021-10-30 22:24:16 +0900

    fixup! Support process.action_mailer

--- test/rails_band/action_mailer/event/process_test.rb
+++ test/rails_band/action_mailer/event/process_test.rb
@@ -58,7 +58,7 @@ class ProcessTest < ActionDispatch::IntegrationTest
 
   test 'calls #to_h' do
     get "/users/#{@user.id}/welcome_email"
-    %i[name time end transaction_id children cpu_time idle_time allocations duration mailer action args ].each do |key|
+    %i[name time end transaction_id children cpu_time idle_time allocations duration mailer action args].each do |key|
       assert_includes @event.to_h, key
     end
   end
