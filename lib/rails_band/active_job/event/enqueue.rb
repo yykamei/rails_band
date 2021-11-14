8b29cd5e73dcdd40c963d6f1a611bdd94bb1ad58
Yutaka Kamei <kamei@yykamei.me>
2021-11-14 22:42:53 +0900

    Support enqueue.active_job

---
+++ lib/rails_band/active_job/event/enqueue.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module RailsBand
+  module ActiveJob
+    module Event
+      # A wrapper for the event that is passed to `enqueue.active_job`.
+      class Enqueue < BaseEvent
+        def adapter
+          @adapter ||= @event.payload.fetch(:adapter)
+        end
+
+        def job
+          @job ||= @event.payload.fetch(:job)
+        end
+      end
+    end
+  end
+end
--- test/dummy/app/controllers/yay_controller.rb
+++ test/dummy/app/controllers/yay_controller.rb
@@ -5,4 +5,9 @@ class YayController < ApplicationController
     YayJob.set(wait: 30.seconds).perform_later(name: 'foo', message: 'Hi')
     head :no_content
   end
+
+  def show
+    YayJob.perform_later(name: 'E!', message: 'This is E.')
+    head :no_content
+  end
 end
