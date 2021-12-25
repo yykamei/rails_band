9e0ac992ac1288ec294bbd67189f140b17053302
Yutaka Kamei <kamei@yykamei.me>
2021-12-25 21:54:01 +0900

    Support transmit_subscription_rejection.action_cable (#52)

--- lib/rails_band/action_cable/log_subscriber.rb
+++ lib/rails_band/action_cable/log_subscriber.rb
@@ -3,6 +3,7 @@
 require 'rails_band/action_cable/event/perform_action'
 require 'rails_band/action_cable/event/transmit'
 require 'rails_band/action_cable/event/transmit_subscription_confirmation'
+require 'rails_band/action_cable/event/transmit_subscription_rejection'
 
 module RailsBand
   module ActionCable
@@ -20,6 +21,10 @@ module RailsBand
 
       def transmit_subscription_confirmation(event)
         consumer_of(__method__)&.call(Event::TransmitSubscriptionConfirmation.new(event))
+      end
+
+      def transmit_subscription_rejection(event)
+        consumer_of(__method__)&.call(Event::TransmitSubscriptionRejection.new(event))
       end
 
       private
