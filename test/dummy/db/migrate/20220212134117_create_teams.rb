f221dc9a0b5e70553f16bfe5daab29723015ceb8
Yutaka Kamei <kamei@yykamei.me>
2022-02-12 23:24:17 +0900

    fixup! Support service_upload.active_storage

--- test/dummy/db/migrate/20220212134117_create_teams.rb
+++ test/dummy/db/migrate/20220212134117_create_teams.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateTeams < ActiveRecord::Migration[7.0]
+class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
       t.string :name, null: false
