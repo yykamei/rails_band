3bc19cb4e997a179eea0356e165f67963926f62a
Yutaka Kamei <kamei@yykamei.me>
2022-03-02 11:48:09 +0900

    route

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Rails.application.routes.default_url_options = { host: 'www.example.com' }
+
 Rails.application.routes.draw do
   resources :users do
     get :flawed
