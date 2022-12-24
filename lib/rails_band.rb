336ecdeb97b03d02c60161cabe7118d899b2bcb4
Yutaka Kamei <kamei@yykamei.me>
2022-12-24 23:42:32 +0900

    Add top-level documentation for each module (#112)

--- lib/rails_band.rb
+++ lib/rails_band.rb
@@ -9,38 +9,47 @@ require 'rails_band/railtie'
 # Rails::Band unsubscribes all default LogSubscribers from Rails Instrumentation API,
 # and it subscribes our own custom LogSubscribers to make it easy to access Rails Instrumentation API.
 module RailsBand
+  # RailsBand::ActionController is responsible for subscribing notifications from ActionController.
   module ActionController
     autoload :LogSubscriber, 'rails_band/action_controller/log_subscriber'
   end
 
+  # RailsBand::ActionView is responsible for subscribing notifications from ActionView.
   module ActionView
     autoload :LogSubscriber, 'rails_band/action_view/log_subscriber'
   end
 
+  # RailsBand::ActiveRecord is responsible for subscribing notifications from ActiveRecord.
   module ActiveRecord
     autoload :LogSubscriber, 'rails_band/active_record/log_subscriber'
   end
 
+  # RailsBand::ActionMailer is responsible for subscribing notifications from ActionMailer.
   module ActionMailer
     autoload :LogSubscriber, 'rails_band/action_mailer/log_subscriber'
   end
 
+  # RailsBand::ActionDispatch is responsible for subscribing notifications from ActionDispatch.
   module ActionDispatch
     autoload :LogSubscriber, 'rails_band/action_dispatch/log_subscriber'
   end
 
+  # RailsBand::ActiveSupport is responsible for subscribing notifications from ActiveSupport.
   module ActiveSupport
     autoload :LogSubscriber, 'rails_band/active_support/log_subscriber'
   end
 
+  # RailsBand::ActiveJob is responsible for subscribing notifications from ActiveJob.
   module ActiveJob
     autoload :LogSubscriber, 'rails_band/active_job/log_subscriber'
   end
 
+  # RailsBand::ActionCable is responsible for subscribing notifications from ActionCable.
   module ActionCable
     autoload :LogSubscriber, 'rails_band/action_cable/log_subscriber'
   end
 
+  # RailsBand::ActiveStorage is responsible for subscribing notifications from ActiveStorage.
   module ActiveStorage
     autoload :LogSubscriber, 'rails_band/active_storage/log_subscriber'
   end
